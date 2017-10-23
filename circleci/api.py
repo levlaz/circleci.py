324367b00345a2a270a9c16f646d06890d9382ef
Lev Lazinskiy <lev@levlaz.org>
2017-10-22 22:16:22 -0700

    Add most important API methods

--- circleci/api.py
+++ circleci/api.py
@@ -33,12 +33,20 @@ class Api():
         self.url = url
 
     def get_user_info(self):
-        """Provides information about the signed in user."""
+        """Provides information about the signed in user.
+
+        Endpoint:
+            GET: /me
+        """
         resp = self._request('GET', 'me')
         return resp
 
     def get_projects(self):
-        """List of all the projects you're following on CircleCI."""
+        """List of all the projects you're following on CircleCI.
+
+        Endpoint:
+            GET: /projects
+        """
         resp = self._request('GET', 'projects')
         return resp
 
@@ -46,13 +54,16 @@ class Api():
         """Follow a new project on CircleCI.
 
         Args:
+            username (str):
+                org or user name
+            project (str):
+                case sensitive repo name
             vcs_type (str):
                 defaults to github
                 on circleci.com you can also pass in bitbucket
-            username (str):
-                github org or user name
-            project (str):
-                case sensitive repo name
+
+        Endpoint:
+            POST: /project/:vcs-type/:username/:project/follow
         """
         endpoint = 'project/{0}/{1}/{2}/follow'.format(
             vcs_type,
@@ -61,6 +72,226 @@ class Api():
         )
         resp = self._request('POST', endpoint)
         return resp
+
+    def get_project_build_summary(self, username, project, vcs_type='github'):
+        """Build summary for each of the last 30 builds for a single git repo.
+
+        Args:
+            username (str):
+                org or user name
+            project (str):
+                case sensitive repo name
+            vcs_type (str):
+                defaults to github
+                on circleci.com you can also pass in bitbucket
+
+        Endpoint:
+            GET: /project/:vcs-type/:username/:project
+        """
+        endpoint = 'project/{0}/{1}/{2}'.format(
+            vcs_type,
+            username,
+            project
+        )
+        resp = self._request('GET', endpoint)
+        return resp
+
+    def get_recent_builds(self):
+        """
+        Build summary for each of the last 30 recent builds, ordered by build_num.
+
+        Endpoint:
+            GET: /recent-builds
+        """
+        resp = self._request('GET', 'recent-builds')
+        return resp
+
+    def get_build_info(self, username, project, build_num, vcs_type='github'):
+        """Full details for a single build.
+
+        Args:
+            username (str):
+                org or user name
+            project (str):
+                case sensitive repo name
+            build_num (str):
+                build number
+            vcs_type (str):
+                defaults to github
+                on circleci.com you can also pass in bitbucket
+
+        Endpoint:
+            GET: /project/:vcs-type/:username/:project/:build_num
+        """
+        endpoint = 'project/{0}/{1}/{2}/{3}'.format(
+            vcs_type,
+            username,
+            project,
+            build_num
+        )
+        resp = self._request('GET', endpoint)
+        return resp
+
+    def get_artifacts(self, username, project, build_num, vcs_type='github'):
+        """List the artifacts produced by a given build.
+
+        Args:
+            username (str):
+                org or user name
+            project (str):
+                case sensitive repo name
+            build_num (str):
+                build number
+            vcs_type (str):
+                defaults to github
+                on circleci.com you can also pass in bitbucket
+
+        Endpoint:
+            GET: /project/:vcs-type/:username/:project/:build_num/artifacts
+        """
+        endpoint = 'project/{0}/{1}/{2}/{3}/artifacts'.format(
+            vcs_type,
+            username,
+            project,
+            build_num
+        )
+        resp = self._request('GET', endpoint)
+        return resp
+
+    def retry_build(self, username, project, build_num, vcs_type='github'):
+        """Retries the build.
+
+        Args:
+            username (str):
+                org or user name
+            project (str):
+                case sensitive repo name
+            build_num (str):
+                build number
+            vcs_type (str):
+                defaults to github
+                on circleci.com you can also pass in bitbucket
+
+        Endpoint:
+            POST: /project/:vcs-type/:username/:project/:build_num/retry
+        """
+        endpoint = 'project/{0}/{1}/{2}/{3}/retry'.format(
+            vcs_type,
+            username,
+            project,
+            build_num
+        )
+        resp = self._request('POST', endpoint)
+        return resp
+
+    def cancel_build(self, username, project, build_num, vcs_type='github'):
+        """Cancels the build.
+
+        Args:
+            username (str):
+                org or user name
+            project (str):
+                case sensitive repo name
+            build_num (str):
+                build number
+            vcs_type (str):
+                defaults to github
+                on circleci.com you can also pass in bitbucket
+
+        Endpoint:
+            POST: /project/:vcs-type/:username/:project/:build_num/cancel
+        """
+        endpoint = 'project/{0}/{1}/{2}/{3}/cancel'.format(
+            vcs_type,
+            username,
+            project,
+            build_num
+        )
+        resp = self._request('POST', endpoint)
+        return resp
+
+    def add_ssh_user(self, username, project, build_num, vcs_type='github'):
+        """Adds a user to the build's SSH permissions.
+
+        Args:
+            username (str):
+                org or user name
+            project (str):
+                case sensitive repo name
+            build_num (str):
+                build number
+            vcs_type (str):
+                defaults to github
+                on circleci.com you can also pass in bitbucket
+
+        Endpoint:
+            POST: /project/:vcs-type/:username/:project/:build_num/ssh-users
+        """
+        endpoint = 'project/{0}/{1}/{2}/{3}/ssh-users'.format(
+            vcs_type,
+            username,
+            project,
+            build_num
+        )
+        resp = self._request('POST', endpoint)
+        return resp
+
+
+    def trigger_build(
+            self,
+            username,
+            project,
+            branch='master',
+            vcs_type='github'):
+        """
+        Triggers a new build.
+
+        # TODO support build params
+        # https://circleci.com/docs/1.0/parameterized-builds/
+
+        Args:
+            username (str):
+                org or user name
+            project (str):
+                case sensitive repo name
+            branch (str):
+                defaults to master
+            vcs_type (str):
+                defaults to github
+                on circleci.com you can also pass in bitbucket
+
+        Endpoint:
+            POST: /project/:vcs-type/:username/:project/tree/:branch
+        """
+        endpoint = 'project/{0}/{1}/{2}/tree/{3}'.format(
+            vcs_type,
+            username,
+            project,
+            branch
+        )
+
+        resp = self._request('POST', endpoint)
+        return resp
+
+
+# TODO support the rest of these methods
+# POST: /project/:vcs-type/:username/:project/ssh-key
+#     Create an ssh key used to access external systems that require SSH key-based authentication
+# GET: /project/:vcs-type/:username/:project/checkout-key
+#     Lists checkout keys.
+# POST: /project/:vcs-type/:username/:project/checkout-key
+#     Create a new checkout key.
+# GET: /project/:vcs-type/:username/:project/checkout-key/:fingerprint
+#     Get a checkout key.
+# DELETE: /project/:vcs-type/:username/:project/checkout-key/:fingerprint
+#     Delete a checkout key.
+# DELETE: /project/:vcs-type/:username/:project/build-cache
+#     Clears the cache for a project.
+# POST: /user/ssh-key
+#     Adds a CircleCI key to your GitHub User account.
+# POST: /user/heroku-key
+#     Adds your Heroku API key to CircleCI, takes apikey as form param name.
+
 
     def _request(self, verb, endpoint):
         """Request a url.
--- tests/circle/test_api.py
+++ tests/circle/test_api.py
@@ -12,7 +12,8 @@ class TestCircleCIApi(unittest.TestCase):
 
     def setUp(self):
         self.c = Api(
-            token = os.getenv('CIRCLE_CI_TOKEN'))
+            token = os.getenv('CIRCLE_CI_TOKEN'),
+            url='https://ccie-preview.sphereci.com/api/v1.1')
 
     def loadMock(self, filename):
         """helper function to open mock responses"""
@@ -41,4 +42,68 @@ class TestCircleCIApi(unittest.TestCase):
 
         self.assertEqual(resp[0]['vcs_url'], 'MOCK+https://ghe-dev.circleci.com/ccie-tester/testing')
 
+    def test_follow_project(self):
+        self.loadMock('mock_follow_project_response')
+        resp = json.loads(self.c.follow_project('ccie-tester', 'testing'))
 
+        self.assertEqual(resp["mock+following"], True)
+
+    def test_get_project_build_summary(self):
+        self.loadMock('mock_project_build_summary_response')
+        resp = json.loads(self.c.get_project_build_summary('ccie-tester', 'testing'))
+
+        self.assertEqual(len(resp), 6)
+        self.assertEqual(resp[0]['username'], 'MOCK+ccie-tester')
+
+    def test_get_recent_builds(self):
+        self.loadMock('mock_get_recent_builds_response')
+        resp = json.loads(self.c.get_recent_builds())
+
+        self.assertEqual(len(resp), 7)
+        self.assertEqual(resp[0]['reponame'], 'MOCK+testing')
+
+    def test_get_build_info(self):
+        self.loadMock('mock_get_build_info_response')
+        resp = json.loads(self.c.get_build_info('ccie-tester', 'testing', '1'))
+
+        self.assertEqual(resp['reponame'], 'MOCK+testing')
+
+    def test_get_artifacts(self):
+        self.loadMock('mock_get_artifacts_response')
+        resp = json.loads(self.c.get_artifacts('ccie-tester', 'testing', '1'))
+
+        self.assertEqual(resp[0]['path'], 'MOCK+raw-test-output/go-test-report.xml')
+
+    def test_retry_build(self):
+        self.loadMock('mock_retry_build_response')
+        resp = json.loads(self.c.retry_build('ccie-tester', 'testing', '1'))
+
+        self.assertEqual(resp['reponame'], 'MOCK+testing')
+
+    def test_cancel_build(self):
+        self.loadMock('mock_cancel_build_response')
+        resp = json.loads(self.c.cancel_build('ccie-tester', 'testing', '11'))
+
+        self.assertEqual(resp['reponame'], 'MOCK+testing')
+        self.assertEqual(resp['build_num'], 11)
+        self.assertTrue(resp['canceled'])
+
+    def test_add_ssh_user(self):
+        self.loadMock('mock_add_ssh_user_response')
+        resp = json.loads(self.c.add_ssh_user('ccie-tester', 'testing', '11'))
+
+        self.assertEqual(resp['reponame'], 'MOCK+testing')
+        self.assertEqual(resp['ssh_users'][0]['login'], 'ccie-tester')
+
+    # def test_add_ssh_key(self):
+    #     resp = self.c.create_ssh_key('ccie-tester', 'testing')
+    #     with open('tests/mocks/mock_create_ssh_key_response', 'w') as f:
+    #          json.dump(resp, f)
+
+    #     print(resp)
+
+    def test_trigger_build(self):
+        self.loadMock('mock_trigger_build_response')
+        resp = json.loads(self.c.trigger_build('ccie-tester', 'testing'))
+
+        self.assertEqual(resp['reponame'], 'MOCK+testing')
