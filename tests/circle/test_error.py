7afa8cef3707b536394524e9329734a34fd6f91d
Lev Lazinskiy <lev@levlaz.org>
2017-10-26 23:53:03 -0700

    add tests for error classes

---
+++ tests/circle/test_error.py
@@ -0,0 +1,29 @@
+# pylint: disable-all
+import unittest
+
+from circleci.error import CircleCIException, BadKeyError, BadVerbError, InvalidFilterError
+
+
+class TestCircleCIError(unittest.TestCase):
+
+    def setUp(self):
+        self.base = CircleCIException('fake')
+        self.key = BadKeyError('fake')
+        self.verb = BadVerbError('fake')
+        self.filter = InvalidFilterError('fake')
+
+    def test_error_implements_str(self):
+        self.assertTrue(self.base.__str__ is not object.__str__)
+
+        string = self.base.__str__()
+
+        self.assertIn('invalid', string)
+
+    def test_verb_message(self):
+        self.assertIn('DELETE', self.verb.message)
+
+    def test_key_message(self):
+        self.assertIn('deploy-key', self.key.message)
+
+    def test_filter_message(self):
+        self.assertIn('running', self.filter.message)
